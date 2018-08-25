fc5723c2da4127e4f5c5646143a8539a16dadd5f
Chris Calo <chris.calo@gmail.com>
2018-08-25 05:10:37 -0400

    Add <Link/> and <Button/> components

---
+++ common/Button.vue
@@ -0,0 +1,31 @@
+<!--
+  Button.vue
+  Author: ccalo@google.com (Chris Calo)
+  -->
+<template>
+  <button type="button" class="Button" v-bind="{ disabled }">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+  export default {
+    name: "Button",
+    props: {
+      disabled: {
+        type: Boolean,
+        default: false,
+      }
+    },
+  };
+</script>
+
+<style scoped>
+  .Button[disabled] {
+    /* visuals */
+    opacity: 0.25;
+    
+    /* interaction */
+    pointer-events: none;
+  }
+</style>
---
+++ common/Link.vue
@@ -0,0 +1,18 @@
+<!--
+  Link.vue
+  Author: ccalo@google.com (Chris Calo)
+  -->
+<template>
+  <a v-bind="{ href }" class="Link">
+    <slot></slot>
+  </a>
+</template>
+
+<script>
+  export default {
+    name: "Link",
+    props: {
+      href: String,
+    },
+  };
+</script>
